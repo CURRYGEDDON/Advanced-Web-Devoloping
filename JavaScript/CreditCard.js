496db96883de60c7a6934cf012312b2ca712a7a3
CURRYGEDDON <patnamnv@email.wofford.edu>
2017-03-07 10:13:45 -0500

    uploading files

---
+++ JavaScript/CreditCard.js
@@ -0,0 +1,11 @@
+/* Created by Nathan Patnam 2-23-17 using Luhn algorithm*/
+
+
+// Return true if the rightmost digit is a valid check digit,
+// false otherwise.
+// digit_string is a string containing digit characters ('0', '1', ..., '9')
+function has_valid_check_digit(digit_string){
+
+
+
+}
